dac6341640bead90f78c3b144a29036399ed206f
Nepoleon97 <nepoleon90@gmail.com>
2022-09-22 11:25:00 +0530

    Initial codes

---
+++ src/test/java/org/fb/GitClass.java
@@ -0,0 +1,16 @@
+package org.fb;
+
+public class GitClass {
+
+	private void test() {
+    System.out.println();
+	}
+	
+	private void verify() {
+    System.out.println();
+	}
+	
+	private void complete() {
+    System.out.println();
+	}
+}
